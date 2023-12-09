eeefc484b11c71a360edccf58e242d654eec8dd2
Marvin <marvin.sp@yahoo.com>
2023-12-09 13:32:47 +0100

    rezepte

---
+++ rezepte.ts
@@ -0,0 +1,46 @@
+type ingredients = string;
+type zubezeitungszeit = number;
+type zutaten = string[];
+type mahlzeit = 'Hauptgericht' | 'Vorspeise' | 'Beilage' | 'Dessert' | 'Snack' | 'Frühstück';
+type rezeptkategorie =
+  | 'Auflauf'
+  | 'Pizza'
+  | 'Reis- oder Nudelsalat'
+  | 'Salat'
+  | 'Salatdressing'
+  | 'Tarte'
+  | 'Fingerfood'
+  | 'Dips'
+  | 'Saucen'
+  | 'Suppe'
+  | 'Klöße'
+  | 'Brot und Brötchen'
+  | 'Brotspeise'
+  | 'Aufstrich'
+  | 'Süßspeise'
+  | 'Eis'
+  | 'Kuchen'
+  | 'Kekse'
+  | 'Torte'
+  | 'Confiserie'
+  | 'Getränke'
+  | 'Shake'
+  | 'Gewürzmischung'
+  | 'Pasten'
+  | 'Studentenküche';
+type anlass =
+  | 'Frühling'
+  | 'Sommer'
+  | 'Herbst'
+  | 'Winter'
+  | 'Für Kinder'
+  | 'Ostern'
+  | 'Halloween'
+  | 'Weihnachten'
+  | 'Silvester'
+  | 'Festlich'
+  | 'Grillen'
+  | 'Camping'
+  | 'Party';
+type hauptzutat = 'Kartoffeln' | 'Gemüse' | 'Reis' | 'Nudeln' | 'Suppe' | 'Kürbis';
+type niveau = 'einfach' | 'mittel' | 'schwierig';
